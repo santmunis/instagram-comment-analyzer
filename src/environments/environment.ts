b7e26529f2ba942b035b40defbd3b8f8a8d0edf7
Nathan Munis <nathan@tg4.com.br>
2021-04-19 00:47:39 -0300

    Retira tokens

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000/api',
-  appId: 931893377665298,
-  superSecretAppToken: 'eb14cab9b17a488b72dcd3dfaa8b262e',
+  appId: 1,
+  superSecretAppToken: '',
   urlSiteRedirect: 'https://www.nathanmunis.site/',
 };
 
